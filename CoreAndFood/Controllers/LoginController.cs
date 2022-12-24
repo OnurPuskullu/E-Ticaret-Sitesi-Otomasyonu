b21c04bac988dcadc76db0b9f8635fa77d848757
OnurPuskullu <105853817+OnurPuskullu@users.noreply.github.com>
2022-12-24 16:53:38 +0300

    E-Ticaret Sitesi Otomasyonu
    E-Trade App

---
+++ CoreAndFood/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using CoreAndFood.Data.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace CoreAndFood.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Index(Admin p)
+        {
+            var datavalue = c.Admins.FirstOrDefault(x => x.UserName == p.UserName && x.Password == p.Password);
+            if (datavalue !=null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.UserName)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Login");
+            
+        }
+    }
+}
---
+++ CoreAndFood/Repositories/FoodRepository.cs
@@ -0,0 +1,36 @@
+﻿using CoreAndFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Repositories
+{
+    public class FoodRepository:GenericRepository<Food>
+    {
+        Context c = new Context();
+        public void FoodAdd(Food f)
+        {
+            c.Foods.Add(f);
+            c.SaveChanges();
+        }
+        public void FoodRemove(Food f)
+        {
+            c.Foods.Remove(f);
+            c.SaveChanges();
+        }
+        public void FoodUpdate(Food f)
+        {
+            c.Foods.Update(f);
+            c.SaveChanges();
+        }
+        public List<Food> FoodList()
+        {
+            return c.Foods.ToList();
+        }
+        public void GetFood(int id)
+        {
+            c.Foods.Find(id);
+        }
+    }
+}
